6ea930d8e9659bc81c54bf681344fa25981faf80
lezagek <elizaveta.chemeriskaya@gmail.com>
2024-06-05 01:36:05 +1000

    Добавлена система рекомендаций

--- test_quality_assessment.py
+++ test_quality_assessment.py
@@ -30,6 +30,9 @@ class TestQualityAssessment(tk.Toplevel):
         self.grid_columnconfigure(0, weight=2)
         self.grid_columnconfigure(1, weight=1)
 
+        # Создаем пользовательский шрифт
+        custom_font = font.Font(family="Golos", size=12)
+
         back_frame = tk.Frame(self, bd=10, bg='#FFFFFF')
         back_frame.grid(row=0, column=0, columnspan=2, sticky='we')
 
@@ -50,24 +53,38 @@ class TestQualityAssessment(tk.Toplevel):
 
         # Фрэйм для подсказки
         help_frame = tk.Frame(self, bd=10, bg='#FFFFFF')
-        help_frame.grid(row=1, column=1, sticky='en', padx=10)
+        help_frame.grid(row=1, column=1, sticky='en')
 
         # Фрэйм для изначальной таблицы
         table_frame = tk.Frame(self, bd=10, bg='#FFFFFF')
-        table_frame.grid(row=2, column=0, sticky='wen')
 
         # Фрэйм для отсортированной таблицы
         sorted_table_frame = tk.Frame(self, bd=10, bg='#FFFFFF')
-        sorted_table_frame.grid(row=3, column=0, sticky='wen')
 
         # Фрэйм для рекомендаций
         info_frame = tk.Frame(self, bd=10)
-        info_frame.grid(row=2, rowspan=2, column=1, sticky='wen', padx=10, pady=10)
+
+        # Текст рекомендаций
+        good_stud_label = tk.Label(info_frame, font=custom_font)
+        bad_stud_label = tk.Label(info_frame, font=custom_font)
+        dif_label = tk.Label(info_frame, font=custom_font)
 
         # Фрэйм для итога
         result_frame = tk.Frame(self, bd=10)
-        result_frame.grid(row=4, column=0, columnspan=2, sticky='wesn', padx=10, pady=10)
 
+        # Текст итога
+        result_label = tk.Label(result_frame, text='ИТОГ', font=custom_font)
+        reconsider_label = tk.Label(result_frame, text='Следует пересмотреть', font=custom_font)
+        result_stud_label = tk.Label(result_frame, font=custom_font)
+        result_dif_label = tk.Label(result_frame, font=custom_font)
+
+        # Таблицы
+        tree = ttk.Treeview(table_frame)
+        vert_scrollbar = ttk.Scrollbar(table_frame, orient="vertical", command=tree.yview)
+        horiz_scrollbar = ttk.Scrollbar(table_frame, orient="horizontal", command=tree.xview)
+        sorted_tree = ttk.Treeview(sorted_table_frame)
+        sorted_vert_scrollbar = ttk.Scrollbar(sorted_table_frame, orient="vertical", command=sorted_tree.yview)
+        sorted_horiz_scrollbar = ttk.Scrollbar(sorted_table_frame, orient="horizontal", command=sorted_tree.xview)
 
         # Загрузка изображения и присвоение к help_label
         help_photo = ImageTk.PhotoImage(Image.open("Текст/Подсказка.png"))
@@ -76,17 +93,39 @@ class TestQualityAssessment(tk.Toplevel):
         help_label.configure(image=help_photo)
         help_label.grid(row=0, column=0, sticky='w')
 
-        # Создаем пользовательский шрифт
-        custom_font = font.Font(family="Golos", size=12)
-
-        # После выбора что анализировать выводится следующий виджет
-        def bind_what_to_analyze(event):
+        def reset_number_to_analyze():
             number_to_analyze_ST_label.grid_forget()
             number_to_analyze_SHT_label.grid_forget()
             number_analyze_combobox.grid_forget()
             analyze_label.grid_forget()
 
             cur_number_analyze.set('')
+
+        def reset_all():
+            table_frame.grid_forget()
+            tree.pack_forget()
+            vert_scrollbar.pack_forget()
+            horiz_scrollbar.pack_forget()
+            sorted_table_frame.grid_forget()
+            sorted_tree.pack_forget()
+            sorted_vert_scrollbar.pack_forget()
+            sorted_horiz_scrollbar.pack_forget()
+            info_frame.grid_forget()
+            good_stud_label.grid_forget()
+            bad_stud_label.grid_forget()
+            dif_label.grid_forget()
+            result_frame.grid_forget()
+            result_label.grid_forget()
+            reconsider_label.grid_forget()
+            result_stud_label.grid_forget()
+            result_dif_label.grid_forget()
+
+
+        # После выбора что анализировать выводится следующий виджет
+        def bind_what_to_analyze(event):
+            reset_number_to_analyze()
+            reset_all()
+
             number_analyze_combobox.grid(row=1, column=1, sticky='w', padx=20)
 
             # Заполнение данными в зависимости от выбора, что анализировать
@@ -115,6 +154,8 @@ class TestQualityAssessment(tk.Toplevel):
 
         # После выбора какой ст/шт анализировать выводится кнопка
         def bind_number_to_analyze(event):
+            reset_all()
+
             analyze_label.grid(row=2, column=0, sticky='w', padx=5, pady=5)
 
 
@@ -136,26 +177,56 @@ class TestQualityAssessment(tk.Toplevel):
 
 
         def analyze():
+            table_frame.grid(row=2, column=0, sticky='wen')
+            sorted_table_frame.grid(row=3, column=0, sticky='wen')
+            info_frame.grid(row=2, rowspan=2, column=1, sticky='wen', padx=10, pady=10)
+            result_frame.grid(row=4, column=0, columnspan=2, sticky='wesn', padx=10, pady=10)
+
             if cur_analyze.get() == 'Сеанс тестирования':
                 ST_id = cur_number_analyze.get()
                 ST_id = ST_id[ST_id.find('(') + 1 : ST_id.find(')')]
                 marks = get_marks_ST(ST_id)
                 data = {}
-                tasks = set()
+                tasks = []
                 ind = ''
+                first_id = marks[0][0]
+                num_stud = 0
+                task_difficulty = {}
+
+                text_good_stud = ''
+                text_bad_stud = ''
+
+                text_result_stud = ''
+                text_result_dif = ''
 
                 for mark in marks:
                     if mark[0] != ind:
                         data[mark[0]] = []
                         ind = mark[0]
+                        num_stud += 1
                         
                     data[mark[0]].append(round(mark[1], 2))
-                    tasks.add(mark[2])
+                    if ind == first_id:
+                        tasks.append(mark[2])
+                        task_difficulty[mark[2]] = mark[3]
                 
-                tasks = sorted(list(tasks))
 
                 print(data)
                 print(tasks)
+                print(num_stud)
+                
+                # Пока не надо. Считаем, что повторов не может быть
+                # Переименование ШТЗ, если есть повторы
+                # for i in range(len(tasks)):
+                #     last_index = len(tasks) - 1 - list(reversed(tasks)).index(tasks[i])
+                #     if i != last_index:
+                #         new_ind = 1
+                #         for j in range(i, last_index + 1):
+                #             tasks[j] = f'{str(tasks[j])}.{new_ind}'
+                #             new_ind += 1
+                
+                # print(tasks)
+                print(task_difficulty)
 
                 df = pd.DataFrame(data=data, index=tasks)
                 df.loc['Xi'] = (df.sum())
@@ -164,30 +235,30 @@ class TestQualityAssessment(tk.Toplevel):
                 df.loc['Ri', 'Xi'] = np.nan
                 print(df)
 
+
                 # Создание таблицы
-                tree = ttk.Treeview(table_frame)
+                for item in tree.get_children():
+                    tree.delete(item)
                 tree['columns'] = tuple(df.columns)
 
                 # Вертикальный скролл
-                vert_scrollbar = ttk.Scrollbar(table_frame, orient="vertical", command=tree.yview)
                 vert_scrollbar.pack(side="right", fill="y")
                 tree.configure(yscrollcommand=vert_scrollbar.set)
 
                 # Горизонтальный скролл
-                horiz_scrollbar = ttk.Scrollbar(table_frame, orient="horizontal", command=tree.xview)
                 horiz_scrollbar.pack(side="bottom", fill="x")
                 tree.configure(xscrollcommand=horiz_scrollbar.set)
 
                 # Добавление заголовков столбцов
-                tree.column('#0', width=40, anchor='c')
+                tree.column('#0', anchor='c')
                 for col in df.columns:
-                    tree.column(col, width=50, anchor='e')
+                    tree.column(col, anchor='e')
                 tree.heading('#0', text='')
                 for col in df.columns:
                     tree.heading(col, text=col)
 
                 # Заполнение таблицы данными из DataFrame с округлением
-                for index, row in df.iterrows():
+                for index, row in df.fillna('').iterrows():
                     values = []
                     for value in row:
                         if isinstance(value, (int, float)):
@@ -202,9 +273,75 @@ class TestQualityAssessment(tk.Toplevel):
                 vert_scrollbar.pack(side="right", fill="y")
                 horiz_scrollbar.pack(side="bottom", fill="x")
 
+                print(df['Xi'])
+                print(df[df['Xi'] >= len(tasks) * 0.8].index)
+                print(df.loc['Ri'][df.loc['Ri'] >= num_stud * 0.8].index)
+                
+                # Пока считать, что выполнили задание отлично = 80%
+                # Рекомендации по студентам, выполнивших все задания
+                ind_good_stud = df[df['Xi'] >= len(tasks) * 0.8].index
+                if len(ind_good_stud) > 1:
+                    text_good_stud = f'Испытуемые {", ".join(ind_good_stud)} успешно выполнили все задания теста. \nТест не дает информации об испытуемых, за исключением того, \nчто для них все задания слишком легкие.'
+                    # Расчёт нового Ri
+                    for ind in ind_good_stud:
+                        row_values = df.loc[ind, :]
+                        df.loc['Ri'] = df.loc['Ri'] - row_values
+                    
+                    # Удаление студентов
+                    df = df.drop(ind_good_stud)
+                    num_stud -= len(ind_good_stud)
+                    
+                elif len(ind_good_stud) == 1:
+                    text_good_stud = f'Испытуемый {ind_good_stud[0]} успешно выполнил все задания теста. \nТест не дает информации об испытуемом, за исключением того, \nчто для него все задания слишком легкие.'
+                    # Расчёт нового Ri
+                    row_values = df.loc[ind_bad_stud[0], :]
+                    df.loc['Ri'] = df.loc['Ri'] - row_values
+
+                    # Удаление студентов
+                    df = df.drop(ind_good_stud)
+                    num_stud -= 1
+                
+                if text_good_stud != '':
+                    good_stud_label['text'] = text_good_stud
+                    good_stud_label.grid(row=0, column=0, sticky='w')
+                
+
+                # Рекомендации по студентам, не выполнивших ни одно задание
+                ind_bad_stud = df[df['Xi'] < len(tasks) * 0.6].index
+                if len(ind_bad_stud) > 1:
+                    text_bad_stud += f'Испытуемые {", ".join(ind_bad_stud)} не прошли ни одно задание теста. \nТест не дает информации об испытуемых, за исключением того, \nчто для них все задания слишком сложные.'
+                    # Расчёт нового Ri
+                    for ind in ind_bad_stud:
+                        row_values = df.loc[ind, :]
+                        df.loc['Ri'] = df.loc['Ri'] - row_values
+
+                    # Удаление студентов
+                    df = df.drop(ind_bad_stud)
+                    num_stud -= len(ind_bad_stud)
+
+                elif len(ind_bad_stud) == 1:
+                    text_bad_stud += f'Испытуемый {ind_bad_stud[0]} не прошёл ни одно задание теста. \nТест не дает информации об испытуемом, за исключением того, \nчто для него все задания слишком сложные.'
+                    # Расчёт нового Ri
+                    row_values = df.loc[ind_bad_stud[0], :]
+                    df.loc['Ri'] = df.loc['Ri'] - row_values
+                    
+                    # Удаление студентов
+                    df = df.drop(ind_bad_stud)
+                    num_stud -= 1
+                    
+                if text_bad_stud != '':
+                    bad_stud_label['text'] = text_bad_stud
+                    bad_stud_label.grid(row=1, column=0, sticky='w')
+
+                
+                print(df.loc['Ri'][df.loc['Ri'] >= num_stud * 0.8].index)
+
+
+
+
                 # Вычитание и деление на кол-во студентов
-                df.loc['Wj'] = (10 - df.loc['Ri'])
-                df.loc['pj'] = (df.loc['Ri'] / 10)
+                df.loc['Wj'] = (num_stud - df.loc['Ri'])
+                df.loc['pj'] = (df.loc['Ri'] / num_stud)
                 df.loc['qj'] = (1 - df.loc['pj'])
                 df.loc['pjqj'] = (df.loc['pj'] * df.loc['qj'])
 
@@ -218,23 +355,22 @@ class TestQualityAssessment(tk.Toplevel):
 
 
                 # Создание отсортированной таблицы
-                sorted_tree = ttk.Treeview(sorted_table_frame)
+                for item in sorted_tree.get_children():
+                    sorted_tree.delete(item)
                 sorted_tree['columns'] = tuple(df.columns)
 
                 # Вертикальный скролл
-                sorted_vert_scrollbar = ttk.Scrollbar(sorted_table_frame, orient="vertical", command=sorted_tree.yview)
                 sorted_vert_scrollbar.pack(side="right", fill="y")
                 sorted_tree.configure(yscrollcommand=sorted_vert_scrollbar.set)
 
                 # Горизонтальный скролл
-                sorted_horiz_scrollbar = ttk.Scrollbar(sorted_table_frame, orient="horizontal", command=sorted_tree.xview)
                 sorted_horiz_scrollbar.pack(side="bottom", fill="x")
                 sorted_tree.configure(xscrollcommand=sorted_horiz_scrollbar.set)
 
                 # Добавление заголовков столбцов
-                sorted_tree.column('#0', width=40, anchor='c')
+                sorted_tree.column('#0', anchor='c')
                 for col in df.columns:
-                    sorted_tree.column(col, width=50, anchor='e')
+                    sorted_tree.column(col, anchor='e')
                 sorted_tree.heading('#0', text='')
                 for col in df.columns:
                     sorted_tree.heading(col, text=col)
@@ -255,9 +391,61 @@ class TestQualityAssessment(tk.Toplevel):
                 sorted_vert_scrollbar.pack(side="right", fill="y")
                 sorted_horiz_scrollbar.pack(side="bottom", fill="x")
 
-                tk.Label(result_frame, text='ИТОГ', font=custom_font).grid(row=0, column=0, sticky='w')
-                tk.Label(result_frame, text='Следует пересмотреть', font=custom_font).grid(row=1, column=0, sticky='w')
 
+                print(task_difficulty)
+                ind_dif = []
+                # Если сложность задания не соответсвует действительности, то запоминаем задание
+                for task in task_difficulty:
+                    print(round(df.loc['qj', task], 2))
+                    if round(df.loc['qj', task], 2) > task_difficulty[task] + 0.1 or round(df.loc['qj', task], 2) < task_difficulty[task] - 0.1:
+                        ind_dif.append(tuple([task, task_difficulty[task], round(df.loc['qj', task], 2)]))
+                    
+                print(ind_dif)
+
+                if len(ind_dif) == 1:
+                    dif_label['text'] = f'У шаблона тестового задания {ind_dif[0][0]} следует изменить сложность (с {ind_dif[0][1]} на [{round(ind_dif[0][2] - 0.1, 2)}, {round(ind_dif[0][2] + 0.1, 2)}])'
+                    dif_label.grid(row=2, column=0, sticky='w')
+                elif len(ind_dif) > 1:
+                    num_task_list = []
+                    dif_task_list = []
+                    for ind in ind_dif:
+                        num_task_list.append(str(ind[0]))
+                        dif_task_list.append(f'(у {ind[0]} с {ind[1]} на [{round(ind[2] - 0.1, 2)}, {round(ind[2] + 0.1, 2)}])')
+                    dif_task_text = '\n'.join(dif_task_list)
+                    dif_label['text'] = f'У шаблонов тестовых заданий {", ".join(num_task_list)} следует изменить сложность \n {dif_task_text}'
+                    dif_label.grid(row=2, column=0, sticky='w')
+
+                # Итог
+                result_label.grid(row=0, column=0, sticky='w')
+
+                res_list = []
+                for ind in ind_good_stud:
+                    stud = get_stud(ind)
+                    res_list.append(f'испытуемого {ind} {stud[0][0]} {stud[0][1]} ({stud[0][2]})')
+                for ind in ind_bad_stud:
+                    stud = get_stud(ind)
+                    res_list.append(f'испытуемого {ind} {stud[0][0]} {stud[0][1]} ({stud[0][2]})')
+                
+                
+                print(res_list)    
+                text_result_stud = '\n'.join(res_list)
+
+                # Добавить проверку на задания
+                if text_result_stud != '':
+                    reconsider_label.grid(row=1, column=0, sticky='w')
+                    result_stud_label['text'] = text_result_stud
+                    result_stud_label.grid(row=2, column=0, sticky='w')
+                
+                res_list = []
+                for ind in ind_dif:
+                    res_list.append(f'шаблон тестового задания {ind[0]}')
+
+                text_result_dif = '\n'.join(res_list)
+
+                if text_result_dif != '':
+                    reconsider_label.grid(row=1, column=0, sticky='w')
+                    result_dif_label['text'] = text_result_dif
+                    result_dif_label.grid(row=3, column=0, sticky='w')
 
 
             elif cur_analyze.get() == 'Шаблон тестирования':
@@ -276,13 +464,10 @@ class TestQualityAssessment(tk.Toplevel):
 
         # Очистка всех виджетов
         def del_all():
-            number_to_analyze_ST_label.grid_forget()
-            number_to_analyze_SHT_label.grid_forget()
-            number_analyze_combobox.grid_forget()
-            analyze_label.grid_forget()
+            reset_number_to_analyze()
+            reset_all()
 
             cur_analyze.set('')
-            cur_number_analyze.set('')
 
 
         # Загрузка изображения и присвоение к del_label
