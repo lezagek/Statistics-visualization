77d1a2c283369783c7c88b3300962f7fa31ab9f0
lezagek <elizaveta.chemeriskaya@gmail.com>
2024-06-04 15:52:30 +1000

    Изменён цвет фона. Кнопки заменены на картинки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 from vars import *
 
@@ -12,14 +13,31 @@ class Main(tk.Frame):
         self.init_main()
 
     def init_main(self):
-        btn_frame = tk.Frame()
+        btn_frame = tk.Frame(bg='#FFFFFF')
         btn_frame.grid(row=0, column=0)
 
-        btn_open_test_quality = tk.Button(btn_frame, text='Оценка качества теста', command=self.open_test_quality, width=20)
-        btn_open_test_quality.grid(sticky='we', pady=10)
 
-        btn_open_test_result = tk.Button(btn_frame, text='Оценка результатов \nтестируемых', command=self.open_test_result)
-        btn_open_test_result.grid(sticky='we', pady=10)
+        # Загрузка изображения и присвоение к test_quality_label
+        test_quality_photo = ImageTk.PhotoImage(Image.open("Кнопки/Оценка_качества_теста.png"))
+        test_quality_label = tk.Label(btn_frame, bg='#FFFFFF')
+        test_quality_label.image = test_quality_photo  # Сохраняем ссылку на изображение, чтобы оно не удалилось из памяти
+        test_quality_label.configure(image=test_quality_photo)
+        test_quality_label.grid(sticky='we', pady=10)
+
+        # Привязываем событие нажатия на картинку к вызову self.open_test_quality
+        test_quality_label.bind('<Button-1>', lambda event: self.open_test_quality())
+
+
+        # Загрузка изображения и присвоение к test_result_label
+        test_result_photo = ImageTk.PhotoImage(Image.open("Кнопки/Оценка_результатов_тестируемых.png"))
+        test_result_label = tk.Label(btn_frame, bg='#FFFFFF')
+        test_result_label.image = test_result_photo  # Сохраняем ссылку на изображение, чтобы оно не удалилось из памяти
+        test_result_label.configure(image=test_result_photo)
+        test_result_label.grid(sticky='we', pady=10)
+
+        # Привязываем событие нажатия на картинку к вызову self.open_test_result
+        test_result_label.bind('<Button-1>', lambda event: self.open_test_result())
+
 
     # Открытие оценки качества теста
     def open_test_quality(self):
@@ -37,7 +55,7 @@ if __name__ == '__main__':
     root.title('ЯDraw')
     root.state('zoomed')
     root.resizable(False, False)
-    root.config()
+    root.config(bg='#FFFFFF')
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
     root.mainloop()
